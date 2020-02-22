a323d27b0a1bd82a69d105f175913a33334fe433
viagram <viagram@users.noreply.github.com>
2020-02-22 14:47:52 +0800

    Update k3screenctrl.lua

--- files/luci/model/cbi/k3screenctrl.lua
+++ files/luci/model/cbi/k3screenctrl.lua
@@ -33,10 +33,11 @@ o = s:option(Value, "city", translate("city :"), translate("please input right c
 --o.rmempty = false
 
 o = s:option(ListValue, "update_time", translate("weather update_time :"))
+o:value("0",translate("off"))
 o:value("3600",translate("1h"))
 o:value("14400",translate("4h"))
 o:value("43200",translate("12h"))
-o.default = 3600
+o.default = 0
 o.rmempty = false
 
 o = s:option(Flag, "psk_hide", translate("Hide Wireless password"))
